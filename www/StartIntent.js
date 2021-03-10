ca89abe751889d6eb94e5a748114c685efa92608
mightymop <totzkotz@gmail.com>
2021-03-10 19:36:46 +0100

    test integration of cordova-plugin-intent

--- www/StartIntent.js
+++ www/StartIntent.js
@@ -3,11 +3,20 @@ var PLUGIN_NAME = 'StartIntent';
 
 var startintent = {
 
-	startActivity : function (params, success, error ) {
+	cordova.startActivity : function (params, success, error ) {
 		exec(success, error, 'StartIntent', 'startActivityFromCordova', [params]);
 	},
-	readDataFromContentUri : function (uri, success, error ) {
+	cordova.readDataFromContentUri : function (uri, success, error ) {
 		exec(success, error, 'StartIntent', 'readDataFromContentUri', [uri]);
+	},
+	cordova.getCordovaIntent : function (success, error) {
+		exec(success, error, 'StartIntent', 'getCordovaIntent', []);
+	},
+	cordova.setNewIntentHandler : function (method ) {
+		exec(method, null, 'StartIntent', 'setNewIntentHandler', [method]);
+	},
+	cordova.getRealPathFromContentUrl : function (uri, success, error ) {
+		exec(success, error, 'StartIntent', 'getRealPathFromContentUrl', [uri]);
 	}
 };
 
